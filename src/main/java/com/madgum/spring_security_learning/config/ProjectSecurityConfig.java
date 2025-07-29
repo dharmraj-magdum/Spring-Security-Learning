101ec3ca16aef7e88b250e4b4a5e06c482137ad5
Dharmraj Magdum <dkmagdum23@gmail.com>
2025-07-29 19:58:26 +0530

    added Learning.txt

--- src/main/java/com/madgum/spring_security_learning/config/ProjectSecurityConfig.java
+++ src/main/java/com/madgum/spring_security_learning/config/ProjectSecurityConfig.java
@@ -26,11 +26,6 @@ public class ProjectSecurityConfig {
         return http.build();
     }
 
-    /*@Bean
-    public UserDetailsService userDetailsService(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
-    }*/
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
